3636bd7dff63a319a19e40ee432f21caa00c4342
nico-alv <nicolas.alvarez02@alumnos.ucn.cl>
2025-01-10 02:17:11 -0300

    fix: update dockerfile and add port env parameter

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,6 +8,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
